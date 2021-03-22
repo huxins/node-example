57455f5fb466b2f5458004a6f87f69a2b3a28814
huxins <huxins@163.com>
2021-03-22 17:53:31 +0800

    utility

--- express/app.js
+++ express/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var utility = require('utility');
+
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+    var q = req.query.q;
+    var result = "Hello World"
+    if (q) {
+        var result = utility.md5(q);
+    }
+    res.send(result);
 });
 
 app.listen(3000, function () {
-  console.log('app is listening at port 3000');
+    console.log('app is running at port 3000');
 });
